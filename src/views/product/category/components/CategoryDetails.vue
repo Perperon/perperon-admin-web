dc22654b66cf17dbbfef33b99eaf53a273fd84b9
Perperon <du1443528055@163.com>
2024-07-12 17:56:27 +0800

    商品管理提交页面

---
+++ src/views/product/category/components/CategoryDetails.vue
@@ -0,0 +1,137 @@
+<template>
+  <!--添加信息框-->
+  <el-dialog
+    :title='dialogTitle'
+    :visible.sync='dialogVisible'
+    width='40%'
+    :before-close='handleBeforeClose'
+    @close='addDialogClosed'>
+    <!--内容区-->
+    <el-form :model='addForm' :rules='addFormRules' ref='addFormRef' label-width='150px'>
+      <el-form-item label='用户名:' prop='username'>
+        <el-input v-model='addForm.username' :disabled="isEdit"></el-input>
+      </el-form-item>
+      <el-form-item label='昵称:' prop='nickName'>
+        <el-input v-model='addForm.nickName' ></el-input>
+      </el-form-item>
+      <el-form-item label='邮箱:' prop='email'>
+        <el-input v-model='addForm.email' ></el-input>
+      </el-form-item>
+      <el-form-item label='是否启用:' prop='status'>
+        <el-radio-group v-model="addForm.status">
+          <el-radio :label="true">启用</el-radio>
+          <el-radio :label="false">禁用</el-radio>
+        </el-radio-group>
+      </el-form-item>
+    </el-form>
+    <!--底部区-->
+    <span slot='footer' class='dialog-footer'>
+      <el-button @click='dialogVisibleBut'>取 消</el-button>
+      <el-button type='primary' @click='onSubmit'>确 定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+import { update, create, getById } from 'api/login'
+
+const defaultFrom = {
+  username: '',
+  nickName: '',
+  status: true,
+  email: ''
+}
+export default {
+  name: 'CategoryDetails',
+  props: {
+    isEdit: {
+      type: Boolean,
+      default: false
+    },
+    isDialog:{
+      type: Boolean,
+      default: false
+    },
+    dialogTitle:{
+      type: String,
+      default: '添加',
+    },
+    id:{
+      type: String,
+      default: null,
+    }
+  },
+  data () {
+    return {
+      dialogVisible: false,
+      addForm: Object.assign({},defaultFrom),
+      addFormRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }],
+        nickName: [
+          { required: true, message: '请输入你的昵称', trigger: 'blur' },
+          { min: 1, max: 30, message: '长度在 1 到 20 个字符', trigger: 'blur' }],
+        email: [
+          { required: true, message: '请输入邮箱', trigger: 'blur' },
+          { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }]
+
+      }
+    }
+  },
+  watch: {
+    isDialog(val) {
+      this.dialogVisible = val
+      if (this.isEdit){
+        getById(this.id).then(response => {
+          this.addForm = response.data
+        })
+      }else{
+        this.addForm = Object.assign({},defaultFrom)
+      }
+    }
+  },
+  methods: {
+    handleBeforeClose(done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          this.dislogVisible = false
+          this.$emit('dislogDetails',this.dislogVisible)
+          done()
+        })
+    },
+    addDialogClosed(){
+      //关闭时重置表单
+      this.$refs.addFormRef.resetFields()
+    },
+    onSubmit(){
+      this.$refs.addFormRef.validate((valid) => {
+        if (!valid)  return
+        if (this.isEdit) {
+          update(this.addForm).then(res => {
+            if (res.code !== 200) return this.$message.error(res.message)
+            this.$message.success(res.message)
+            this.dialogVisible = false
+            this.$emit('dislogDetails',this.dislogVisible)
+          })
+        }else{
+          create(this.addForm).then(res => {
+            if (res.code !== 200) return this.$message.error(res.message)
+            this.$message.success(res.message)
+            this.dialogVisible = false
+            this.$emit('dislogDetails',this.dislogVisible)
+          })
+        }
+      })
+    },
+    dialogVisibleBut(){
+      this.dislogVisible = false
+      this.$emit('dislogDetails',this.dislogVisible)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/product/category/index.vue
+++ src/views/product/category/index.vue
@@ -1,12 +1,172 @@
 <template>
-<div>
-  商品分类
-</div>
+  <div class='app-container'>
+    <div class='table-container'>
+      <el-row :gutter='20'>
+        <el-col :span='16'>
+          <el-button v-has="'product:add'" @click='addDialog' class='btnClass'>
+            添加分类
+          </el-button>
+        </el-col>
+      </el-row>
+
+      <el-card>
+        <!--列表区-->
+        <el-table
+          ref="multipleTable"
+          :data='categoryData'
+          border
+          stripe
+          style='width: 100%'
+          @selection-change='handleSelectionChange'
+          :default-sort="{prop: 'created', order: 'descending'}">
+          <el-table-column type='expand'>
+            <template slot-scope='props'>
+              <el-form label-position='left' inline class='from-table'>
+                <el-form-item label='用户账号'>
+                  <span>{{ props.row.username }}</span>
+                </el-form-item>
+                <el-form-item label='邮箱'>
+                  <span>{{ props.row.email }}</span>
+                </el-form-item>
+                <el-form-item label='用户昵称'>
+                  <span>{{ props.row.nickName }}</span>
+                </el-form-item>
+                <el-form-item label='所属角色'>
+                  <span>{{ props.row.roles }}</span>
+                </el-form-item>
+                <el-form-item label='状态'>
+                  <span>{{ props.row.status ? '启用' : '禁用' }}</span>
+                </el-form-item>
+                <el-form-item label='创建时间'>
+                  <span>{{ props.row.created }}</span>
+                </el-form-item>
+              </el-form>
+            </template>
+          </el-table-column>
+          <el-table-column type='selection' width='55'>
+          </el-table-column>
+<!--          <el-table-column label='头像' width='80' align='center'>-->
+<!--            <template slot-scope='scope'>-->
+<!--              <img :src='host+scope.row.icon' width='50' height='50'/>-->
+<!--            </template>-->
+<!--          </el-table-column>-->
+          <el-table-column prop='username' label='账号' align='center'>
+          </el-table-column>
+          <el-table-column prop='email' label='邮箱' align='center'>
+          </el-table-column>
+          <el-table-column prop='nickName' label='昵称' align='center'>
+          </el-table-column>
+<!--          <el-table-column label='状态' align='center'>
+            <template slot-scope='scope'>
+              <el-switch
+                v-model='scope.row.status' @change='updateStatus(scope.row.id,scope.row.status)'>
+              </el-switch>
+            </template>
+          </el-table-column>-->
+          <el-table-column prop='created' label='创建时间' sortable align='center'>
+          </el-table-column>
+          <el-table-column label='操作' width='250'>
+            <template slot-scope='scope'>
+              <el-tooltip class='item' effect='dark' content='编辑' placement='top' v-has="'account:update'">
+                <el-button
+                  size='mini'
+                  type='primary' round
+                  @click='editDialog(scope.$index,scope.row.id)'>
+                  <svg-icon icon-class='edit'></svg-icon>
+                </el-button>
+              </el-tooltip>
+              <el-tooltip class='item' effect='dark' content='删除' placement='top' v-has="'account:delete'">
+                <el-button
+                  size='mini'
+                  type='danger' round
+                  @click="deleteHandler(scope.$index,scope.row.id)">
+                  <svg-icon icon-class='delete'></svg-icon>
+                </el-button>
+              </el-tooltip>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-card>
+      <category-details :is-edit='isEdit' :dialog-title='dialogTitle' :table-data='menuData' :is-dialog='isDialog' :id='id'
+                    @dislogDetails='handleDialog'></category-details>
+    </div>
+  </div>
 </template>
 
 <script>
+import CategoryDetails from './components/CategoryDetails'
 export default {
-  name: 'index'
+  name: 'index',
+  components:{
+    CategoryDetails
+  },
+  data() {
+    return {
+      id: null,
+      categoryData: [],
+      multipleSelection: [],
+      params: Object.assign({
+        status: null,
+      }, params),
+      isEdit: false,
+      isDialog: false,
+      dialogTitle: null
+    }
+  },
+  created() {
+    this.initList()
+  },
+  methods: {
+    initList(queryInfo) {
+      listByPage(queryInfo).then(res => {
+        if (res.code !== 200) return this.$message.error(res.message)
+        console.log(res.data.list)
+        this.accountData = res.data.list
+        this.params.total = res.data.total
+      }).catch(err => {
+        this.$message.error(err)
+      })
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val
+      if (this.multipleSelection.length>0){
+        this.btnFlag = false
+      }else {
+        this.btnFlag = true
+      }
+    },
+    addDialog() {
+      this.isDialog = true;
+      this.isEdit = false;
+      this.dialogTitle = '添加用户'
+    },
+    editDialog(index, id) {
+      this.isDialog = true;
+      this.isEdit = true;
+      this.id = id;
+      this.dialogTitle = '修改用户'
+    },
+    handleDialog(flag){
+      this.isDialog = flag;
+      this.initList(this.params)
+    },
+    deleteHandler(index,id){
+      this.$confirm('确认要删除此用户', '删除提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        deleteById(id).then(response => {
+          this.$message({
+            message: '删除成功',
+            type: 'success',
+            duration: 1000
+          });
+          this.initList(this.params);
+        })
+      })
+    },
+  }
 }
 </script>
 
