39d6a334f730b950376d0e849dfc11f6b968a39b
Perperon <du1443528055@163.com>
2024-07-29 18:00:00 +0800

    商品添加页面调整

--- src/views/product/components/ProductAdd.vue
+++ src/views/product/components/ProductAdd.vue
@@ -4,7 +4,7 @@
       <el-alert
         title='添加商品信息' center show-icon type='info' :closable='false'>
       </el-alert>
-      <el-steps :space='250' :active="1" finish-status="success" align-center>
+      <el-steps :space='250' :active="active - 0" finish-status="success" align-center>
         <el-step title="基本信息" ></el-step>
         <el-step title="商品参数" ></el-step>
         <el-step title="商品属性" ></el-step>
@@ -12,13 +12,27 @@
         <el-step title="商品内容" ></el-step>
         <el-step title="完成" ></el-step>
       </el-steps>
+      <el-tabs v-model='active' tab-position="left" style="height: 200px;">
+        <el-tab-pane label="基本信息" name='0'>基本信息</el-tab-pane>
+        <el-tab-pane label="商品参数" name='1'>商品参数</el-tab-pane>
+        <el-tab-pane label="商品属性" name='2'>商品属性</el-tab-pane>
+        <el-tab-pane label="商品图片" name='3'>商品图片</el-tab-pane>
+        <el-tab-pane label="商品内容" name='4'>商品内容</el-tab-pane>
+      </el-tabs>
     </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ProductAdd'
+  name: 'ProductAdd',
+  data(){
+    return {
+      active: 0
+    }
+  },
+  methods: {
+  }
 }
 </script>
 
